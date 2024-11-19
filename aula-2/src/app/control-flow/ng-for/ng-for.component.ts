edf88642a8bc25593f1c99fbb815c98c7358b925
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-11-19 00:47:02 -0300

    Use @for

--- aula-2/src/app/control-flow/ng-for/ng-for.component.ts
+++ aula-2/src/app/control-flow/ng-for/ng-for.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ng-for',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.scss'
 })

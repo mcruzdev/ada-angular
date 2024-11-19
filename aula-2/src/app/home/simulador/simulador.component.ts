51f3f7b9c5bd1cc75b921a10bf5581c5c37d0545
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-11-19 00:57:52 -0300

    Add documentation about signals

--- aula-2/src/app/home/simulador/simulador.component.ts
+++ aula-2/src/app/home/simulador/simulador.component.ts
@@ -42,7 +42,6 @@ export class SimuladorComponent implements OnDestroy {
 
   quantidade: WritableSignal<number> = signal(0); //caixinha, wrapper
 
-  
   lucro: Signal<number> = computed(() => {
     console.log("Executando pela primeira vez");
     return (
@@ -67,62 +66,3 @@ interface Evento {
   custo: number;
   preco: number;
 }
-
-// let a = 10
-// let b = 20
-// let z = a + b
-// console.log(z) // 30
-
-// a = 20
-// z = a + b
-
-// console.log(z) //
-
-// 1. usando imperativa e nao usando reativo
-// quantidade: number = 0
-// lucro: number = 0
-
-// inc() {
-//   this.quantidade = this.quantidade + 1
-//   this.calcularCusto()
-// }
-
-// dim() {
-//   this.quantidade = this.quantidade - 1
-//   this.calcularCusto()
-// }
-
-// calcularCusto() {
-//   const atual = this.quantidade * this.evento.preco
-//   this.lucro = atual - this.evento.custo;
-// }
-
-// 2. reativa e usando rxjs declarativo
-
-// constructor() {
-//   this.quantidadeSubject.asObservable().subscribe((novoValor) => {
-//     console.log('>>>', novoValor)
-//   });
-
-// }
-
-// private quantidadeSubject = new BehaviorSubject(0);
-
-// quantidade$ = this.quantidadeSubject.asObservable();
-
-// lucro$ = this.quantidadeSubject.pipe( // eu vou realizar algumas operacoes
-//   map((novaQuantidade) => { // operador map do rxjs (lib reativa)
-//   return (novaQuantidade * this.evento.preco) - this.evento.custo;
-// }))
-
-// inc() {
-//   const valorAtual = this.quantidadeSubject.getValue();
-//   this.quantidadeSubject.next(valorAtual + 1);
-
-//   // this.quantidade = this.quantidade + 1;
-// }
-
-// dim() {
-//   const valorAtual = this.quantidadeSubject.getValue();
-//   this.quantidadeSubject.next(valorAtual - 1);
-// }
